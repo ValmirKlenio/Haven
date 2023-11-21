eddeefd7dad6c1f1b47d21971c54d71bbde458b8
samyrafariass <104604760+samyrafariass@users.noreply.github.com>
2023-11-20 21:10:30 -0300

    Update modulo2.py

--- Funcionarios/modulo2.py
+++ Funcionarios/modulo2.py
@@ -1,10 +1,10 @@
 itens_cadastrados = []
     
-def avaliar_doacao(item, condicao, pontuacao itens_cadastrados):
+def avaliar_doacao(item, condicao, itens_cadastrados):
   condicoes_aceitaveis = ["novo", "usado", "em bom estado"]
   item = input('Item que dese doar: ')
-  condicoes = input('Qual a condição do item? ')
-  while condicoes.lower() not in condicoes_aceitaveis:
+  condicao = input('Qual a condição do item? ')
+  while condicao.lower() not in condicoes_aceitaveis:
     print('Desculpe, a condição do item não é aceitável para doação.')
     return False
   if item.lower() in itens_cadastrados:
@@ -18,10 +18,8 @@ def avaliar_doacao(item, condicao, pontuacao itens_cadastrados):
 item_doado = "roupa"
 condicao_item = "usado"
 itens_desejados = ["roupa", "brinquedo", "alimento"]
-
 doacao = avaliar_doacao(item_doado, condicao_item, itens_desejados)
-
 if doacao:
-  print('Pode doar o item)
+  print('Pode doar o item')
 else:
   print('Infelizmente, o item não pode ser doado neste momento.')
